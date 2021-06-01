95dcf35ff5d2a574bcb9154b58469a9997bd8e63
gong-1997 <67048885+gong-1997@users.noreply.github.com>
2021-06-01 18:13:46 +0800

    Add files via upload

---
+++ AdvancedEAST-master/losses.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+
+import cfg
+
+
+def quad_loss(y_true, y_pred):
+    """
+    改进的损失函数
+    y_true: 真实值，由数据集给出
+    y_pred：预测值，由East模型给出
+    """
+    # inside_score 的损失
+    logits = y_pred[:, :, :, :1]
+    labels = y_true[:, :, :, :1]
+    # 平衡图像中的正负样本
+    beta = 1 - tf.reduce_mean(labels)
+    # 首先使用 sigmoid 作为激活函数
+    predicts = tf.nn.sigmoid(logits)
+    # log +epsilon 稳定校准
+    inside_score_loss = tf.reduce_mean(
+        -1 * (beta * labels * tf.log(predicts + cfg.epsilon) +
+              (1 - beta) * (1 - labels) * tf.log(1 - predicts + cfg.epsilon)))
+    inside_score_loss *= cfg.lambda_inside_score_loss
+
+    # side_vertex_code 的损失
+    vertex_logits = y_pred[:, :, :, 1:3]
+    vertex_labels = y_true[:, :, :, 1:3]
+    vertex_beta = 1 - (tf.reduce_mean(y_true[:, :, :, 1:2])
+                       / (tf.reduce_mean(labels) + cfg.epsilon))
+    vertex_predicts = tf.nn.sigmoid(vertex_logits)
+    pos = -1 * vertex_beta * vertex_labels * tf.log(vertex_predicts +
+                                                    cfg.epsilon)
+    neg = -1 * (1 - vertex_beta) * (1 - vertex_labels) * tf.log(
+        1 - vertex_predicts + cfg.epsilon)
+    positive_weights = tf.cast(tf.equal(y_true[:, :, :, 0], 1), tf.float32)
+    side_vertex_code_loss = \
+        tf.reduce_sum(tf.reduce_sum(pos + neg, axis=-1) * positive_weights) / (
+                tf.reduce_sum(positive_weights) + cfg.epsilon)
+    side_vertex_code_loss *= cfg.lambda_side_vertex_code_loss
+
+    # side_vertex_coord delta 的损失
+    g_hat = y_pred[:, :, :, 3:]
+    g_true = y_true[:, :, :, 3:]
+    vertex_weights = tf.cast(tf.equal(y_true[:, :, :, 1], 1), tf.float32)
+    pixel_wise_smooth_l1norm = smooth_l1_loss(g_hat, g_true, vertex_weights)
+    side_vertex_coord_loss = tf.reduce_sum(pixel_wise_smooth_l1norm) / (
+            tf.reduce_sum(vertex_weights) + cfg.epsilon)
+    side_vertex_coord_loss *= cfg.lambda_side_vertex_coord_loss
+    return inside_score_loss + side_vertex_code_loss + side_vertex_coord_loss
+
+
+def smooth_l1_loss(prediction_tensor, target_tensor, weights):
+    """
+    平滑L1损失函数
+    prediction_tensor: 模型预测得到结果，以张量存在
+    target_tensor：真实值，由原始数据转换为张量形式，以张量存在
+    weights
+    """
+    n_q = tf.reshape(quad_norm(target_tensor), tf.shape(weights))
+    diff = prediction_tensor - target_tensor
+    abs_diff = tf.abs(diff)
+    abs_diff_lt_1 = tf.less(abs_diff, 1)
+    pixel_wise_smooth_l1norm = (tf.reduce_sum(
+        tf.where(abs_diff_lt_1, 0.5 * tf.square(abs_diff), abs_diff - 0.5),
+        axis=-1) / n_q) * weights
+    return pixel_wise_smooth_l1norm
+
+
+def quad_norm(g_true):
+    shape = tf.shape(g_true)
+    delta_xy_matrix = tf.reshape(g_true, [-1, 2, 2])
+    diff = delta_xy_matrix[:, 0:1, :] - delta_xy_matrix[:, 1:2, :]
+    square = tf.square(diff)
+    distance = tf.sqrt(tf.reduce_sum(square, axis=-1))
+    distance *= 4.0
+    distance += cfg.epsilon
+    return tf.reshape(distance, shape[:-1])
